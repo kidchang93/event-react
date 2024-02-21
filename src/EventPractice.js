0487c9a46aae57b28f906335b06c04e2bf990da4
kidchang93 <lck930721@gmail.com>
2024-02-21 09:59:31 +0900

    상태값확인

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class EventPractice extends Component {
-  state = {
-    message: '',
-    username:''
-  }
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value })
+	state = {
+		message: "",
+		username: "",
+	};
+	handleChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
-  }
-
-
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input type='text' name='message' placeholder='입력해 주세요.'
-          onChange={this.handleChange}
-        />
-        <br />
-        message: {this.state.message}
-      <br/>
-        <input type='text' name='username' placeholder='입력해 주세요.'
-          onChange={this.handleChange}
-        />
-        <br />
-        username: {this.state.username}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>이벤트 연습</h1>
+				<input
+					type="text"
+					name="message"
+					placeholder="입력해 주세요."
+					onChange={this.handleChange}
+				/>
+				<br />
+				message: {this.state.message}
+				<br />
+				<hr />
+				<input
+					type="text"
+					name="username"
+					placeholder="입력해 주세요."
+					onChange={this.handleChange}
+				/>
+				<br />
+				username: {this.state.username}
+			</div>
+		);
+	}
 }
 
 export default EventPractice;
--- src/EventPracticeFunction.js
+++ src/EventPracticeFunction.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const EventPracticeFunction = () => {
-	const [message, setMessage] = useState("");
+	const [form, setForm] = useState({ message: "", username: "" });
+	// useState 가 계속 만들어지는 불편함이 있으므로
+	// form 이란 object로 받고 비구조할당으로 꺼낸다.
+	const { message, username } = form;
 	const handleChange = (e) => {
-		setMessage(e.target.value);
+		setForm({ ...form, [e.target.name]: e.target.value });
+		// [e.target.name] 라고 써주는거는 e.target.name 자체가 키로 인식하지 못해서 키로 인식하게 저렇게 감싸서 상수로 써준다.
+		console.log("setform", message, username); // 현재 상태값이 찍히지 않음
+		console.log("object", e.target.value); // 현재 상태값이 찍힘
 	};
 
 	return (
@@ -17,6 +23,15 @@ const EventPracticeFunction = () => {
 			/>
 			<br />
 			message: {message}
+			<hr />
+			<input
+				type="text"
+				name="username"
+				placeholder="입력해 주세요."
+				onChange={handleChange}
+			/>
+			<br />
+			username: {username}
 		</div>
 	);
 };
